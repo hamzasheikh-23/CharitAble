71f611aa81a388f11eb84a211be8b8cc058eaf8c
hamzasheikh-23 <hamzashayk@hotmail.com>
2022-01-04 03:12:54 +0500

    add donation functionality with db support

--- app/src/main/java/com/example/charitable/HomeActivity.java
+++ app/src/main/java/com/example/charitable/HomeActivity.java
@@ -6,19 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button logoutBTN;
+    Button addNewDonationBTN,manageDonationBTN,viewDonationBTN,logoutBTN;
+    ImageView change_image, profile_image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        addNewDonationBTN = (Button) findViewById(R.id.add_donation_btn);
+        manageDonationBTN = (Button) findViewById(R.id.manage_donation_btn);
+        viewDonationBTN = (Button) findViewById(R.id.view_donation_btn);
         logoutBTN = (Button) findViewById(R.id.logout_btn);
+
+        change_image = (ImageView) findViewById(R.id.change_profile_image);
+        profile_image = (ImageView) findViewById(R.id.profile_image);
+
+        change_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
+                startActivity(intent);            }
+        });
+
+        addNewDonationBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this,AddNewDonation.class);
+                startActivity(intent);
+            }
+        });
 
         logoutBTN.setOnClickListener(new View.OnClickListener() {
             @Override
